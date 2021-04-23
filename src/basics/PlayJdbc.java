2d7ed8c936d9f016e8881945b5c3396f7f48fe69
Aryan Akshay <varyanakshay@gmail.com>
2021-04-23 09:42:44 +0530

    injecting dependencies through spring

--- src/basics/PlayJdbc.java
+++ src/basics/PlayJdbc.java
@@ -1,4 +1,7 @@
 package basics;   
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import basics.dao.StudentDao;
 import basics.dao.StudentDaoImp;
 import basics.model.*;
@@ -6,12 +9,14 @@ import basics.model.*;
 public class PlayJdbc {
 
 	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Student myStudent = new Student(3, "sanjay", 4, 75);
 		Student anotherStudent= new Student(4, "subbu", 1, 90);
-		Student anotherStudent1= new Student(5, "subbu1", 3, 55);
-		StudentDao studentDao = new StudentDaoImp();
-		studentDao.insert(myStudent);
-		
+		Student anotherStudent4= new Student(8, "verdu", 5, 75);
+		/*Student anotherStudent2= new Student(6, "rakesh", 1, 15);
+		Student anotherStudent3= new Student(7, "harish", 8, 16);*/
+		StudentDao studentDao = (StudentDao) context.getBean("studentDao");
+		studentDao.insert(anotherStudent4);
 			
 		}	
 	}
--- src/basics/dao/StudentDaoImp.java
+++ src/basics/dao/StudentDaoImp.java
@@ -8,9 +8,14 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import basics.model.Student;
 
 public class StudentDaoImp implements StudentDao {
-	
-	private JdbcTemplate jdbcTemplate= new JdbcTemplate(getDataSource());
 
+	private JdbcTemplate jdbcTemplate;
+
+
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 	@Override
 	public void insert(Student student) {
 		String sql = "INSERT INTO student VALUES (?,?,?,?)";
@@ -20,14 +25,9 @@ public class StudentDaoImp implements StudentDao {
 		System.out.println("no of rows inserted  is "+ no_rows_inserted);
 	}
 
-	public DataSource getDataSource() {
-		String url = "jdbc:mysql://127.0.0.1:3306/spring_jdbc";
-		String username = "root";
-		String password ="";
 
-		DriverManagerDataSource dataSource = new DriverManagerDataSource(url, username, password);
-		return dataSource;
-	}
+
 
 
 }
+
